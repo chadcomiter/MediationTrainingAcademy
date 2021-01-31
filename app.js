c5fb1c1b83a0245707889c83aa8f21daf9c9f5b6
Chad Comiter <52842848+chadcomiter@users.noreply.github.com>
2021-01-31 13:23:46 -0500

    Updated 31Jan2021

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ var tenDayProgram = document.querySelector('#tenDayProgram-btn');
 var programNav = document.querySelector('#program-btn');
 var faqNav = document.querySelector('#faq-btn-nav');
 var instructorNav = document.querySelector('#instructor-btn');
-var header = document.querySelector('#v-header');
+
 //Event Listeners
 
 ourPrograms.addEventListener('click', function(){
@@ -77,9 +77,7 @@ instructorNav.addEventListener('click', function(){
 tenDayProgram.addEventListener('click', function(){
     smoothScroll('.program2', 850);
 });
-header.addEventListener('click', function(){
-    smoothScroll('.fullscreen-video-wrap', 850);
-});
+
 
 const main = () => {
     navSlide();
